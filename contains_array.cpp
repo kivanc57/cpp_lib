bb41d37e3959909a9c322aa44a723e8c4bd210bb
Kivanc Gordu <kivanc.gordu@hotmail.com>
2025-10-10 14:14:43 +0200

    initialize: project

---
+++ contains_array.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+bool contains_array(const std::vector<std::string>& seq, const std::string& elem){
+
+    for(unsigned int i = 0; i < seq.size(); i++){
+        if(seq[i] == elem)
+            return true;
+    }
+    return false;
+}
+
+int main(int /*arc*/, char** /*argv[]*/){
+    std::vector<std::string> words = {"apple", "banana", "kiwi"};
+    std::cout << std::boolalpha;
+
+    std::cout << "Check kiwi: "  << contains_array(words, "kiwi") << std::endl;
+    return 0;
+}
---
+++ sum_array.cpp
@@ -0,0 +1,34 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+
+int sum_array(const vector<int> numbers)
+{
+    if (numbers.size() == 0)
+      return 0;
+
+    int sum = 0;
+    int highestNum = numbers[0];
+    int lowestNum = numbers[0];
+
+    for(unsigned int i = 0; i < numbers.size(); i++){
+      if (numbers[i] > highestNum){
+          highestNum = numbers[i];
+      } else if (numbers[i] < lowestNum){
+          lowestNum = numbers[i];
+      }
+    }
+    for(unsigned int i = 0; i < numbers.size(); i++){
+        if(numbers[i] != highestNum && numbers[i] != lowestNum){
+            sum += numbers[i];
+        }
+    }
+    return sum;
+}
+
+int main(int /*argc*/, char** /*argv[]*/) {
+    vector<int> nums = {1, 2, 3, 4, 5};
+    cout << sum_array(nums) << std::endl;
+    return 0;
+}
+
