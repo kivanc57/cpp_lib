f41080acd3ac3cea3a237a42f459c4b8742e9c13
Kivanc Gordu <kivanc.gordu@hotmail.com>
2025-10-10 15:02:25 +0200

    add: square_sum.cpp

---
+++ square_sum.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <iostream>
+
+int square_sum(const std::vector<int>& numbers){
+    int squared_sum = 0;
+
+    for(unsigned int i = 0; i < numbers.size(); i++){
+        squared_sum += numbers[i] * numbers[i];
+    }
+    return squared_sum;
+}
+
+int main(int /*argc*/, char** /*argv[]*/){
+    std::vector<int> numbers = {1, 2, 2};
+    std::cout << square_sum(numbers) << std::endl;
+    return 0;
+}
